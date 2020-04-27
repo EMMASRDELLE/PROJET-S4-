8c18ec9121c09412bd2a4d90d7ae4f11abe06a1b
EMMASRDELLE <57494119+EMMASRDELLE@users.noreply.github.com>
2020-04-27 10:10:07 +0200

    Update Sommet.h

---
+++ PROJET/Sommet.h
@@ -0,0 +1,24 @@
+#ifndef SOMMET_H_INCLUDED
+#define SOMMET_H_INCLUDED
+
+class Sommet
+{
+    public:
+        Sommet(int indice, char nom, int x, int y);
+        virtual ~Sommet();
+        char getNom()const;
+        int getX() const;
+        int getY () const;
+
+
+    protected:
+
+    private:
+
+        int m_indice;
+        char m_nom;
+        int m_x;
+        int m_y;
+};
+
+#endif // SOMMET_H_INCLUDED
