87a9cadd7f8d0922561caae973abc523f527a4ef
EMMASRDELLE <57494119+EMMASRDELLE@users.noreply.github.com>
2020-04-27 10:08:39 +0200

    Arrete

---
+++ PROJET/Arete.h
@@ -0,0 +1,27 @@
+#ifndef ARETE_H_INCLUDED
+#define ARETE_H_INCLUDED
+
+#include "Sommet.h"
+
+class Arrete
+{
+    public:
+        Arrete(int indice, Sommet* ex1, Sommet* ex2);
+        virtual ~Arrete();
+
+       const Sommet* getEx1()const;
+       const Sommet* getEx2()const;
+
+    protected:
+
+    private:
+        int m_indice;
+        Sommet* m_ex1;
+        Sommet* m_ex2;
+
+};
+
+#endif // ARRETE_H
+
+
+#endif // ARETE_H_INCLUDED
