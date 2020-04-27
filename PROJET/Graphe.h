96bb26f97b74901ee0be8b316ad65156ee4ec8c0
EMMASRDELLE <57494119+EMMASRDELLE@users.noreply.github.com>
2020-04-27 10:12:14 +0200

    Update Graphe.h

---
+++ PROJET/Graphe.h
@@ -0,0 +1,30 @@
+#ifndef GRAPHE_H_INCLUDED
+#define GRAPHE_H_INCLUDED
+
+#include "Sommet.h"
+#include "Arrete.h"
+#include <vector>
+#include <iostream>
+#include <fstream>
+class Graphe
+{
+    public:
+
+        /// Méthodes
+
+        Graphe(std::string nomFichier); ///Construction du graphe par lecture de fichier
+        virtual ~Graphe();
+        void afficher() const;
+
+    protected:
+
+    private:
+
+        ///Initialisation des variables
+
+        int m_orientation;
+        std::vector <Sommet*> m_sommets;
+        std::vector<Arrete*> m_arretes;
+};
+
+#endif // GRAPHE_H_INCLUDED
