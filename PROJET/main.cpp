35f50e815b5899e42ee578d5b96350b1314c77c6
EMMASRDELLE <57494119+EMMASRDELLE@users.noreply.github.com>
2020-04-28 13:19:16 +0200

    LOANNE + YANIS

--- PROJET/main.cpp
+++ PROJET/main.cpp
@@ -14,13 +14,15 @@ void afficher_Arbre(std::vector<const Arrete*> arretes)
 int main()
 {
    Svgfile svgout;
+   std::vector <int> DJ;
    Graphe g {"Graphe.txt"};
    g.ChargementFichierPond("Pondération.txt");
    g.afficher();
    g.Dessiner(svgout);
    g.affichage_Resultat1();
     //g.VectorPropre("Resultat2");
-    g.Djikstra(1);
+    DJ=g.Djikstra(1);
+    int id;
 
     return 0;
 }
